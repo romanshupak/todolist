3dcaf59159604a98b150fdaea7a861c3ed328ba2
Roman Shupak <romanshupak808@gmail.com>
2024-11-15 13:29:03 +0200

    Add views

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from tasks.models import Task
+
+
+def index(request):
+    """View function for the home page of the site."""
+    tasks = Task.objects.all()
+
+    context = {"tasks": tasks}
+    return render(request, "tasks/index.html", context=context)
