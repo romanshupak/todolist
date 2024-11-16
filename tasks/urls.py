9e01931e559fefaef3e02d1126cd5bd62341bdbd
Roman Shupak <romanshupak808@gmail.com>
2024-11-16 17:00:20 +0200

    Add url for creating of new task.

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
-from .views import TagsListView
+from .views import TagsListView, CreateTaskView
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("tags/", TagsListView.as_view(), name="tags-list")
+    path("tags/", TagsListView.as_view(), name="tags-list"),
+    path("tasks-create/", views.CreateTaskView.as_view(), name="tasks-create"),
+
 ]
 
 app_name = "tasks"
