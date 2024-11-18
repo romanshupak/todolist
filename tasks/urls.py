d44831a827143cc822bc9561650a39b0ebf7ab01
Roman Shupak <romanshupak808@gmail.com>
2024-11-18 20:54:10 +0200

    Add additional urls in order to create, update and delete pages.

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,11 +1,25 @@
 from django.urls import path
-from . import views
-from .views import TagsListView, CreateTaskView
+from tasks.views import (
+    index,
+    TagsListView,
+    CreateTaskView,
+    UpdateTaskView,
+    DeleteTaskView,
+    CreateTagView,
+    DeleteTagView,
+    UpdateTagView,
+)
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", index, name="index"),
     path("tags/", TagsListView.as_view(), name="tags-list"),
-    path("tasks-create/", views.CreateTaskView.as_view(), name="tasks-create"),
+    path("tasks-create/", CreateTaskView.as_view(), name="tasks-create"),
+    path("tasks-update/<int:pk>/", UpdateTaskView.as_view(), name="tasks-update"),
+    path("tasks-delete/<int:pk>/", DeleteTaskView.as_view(), name="tasks-delete"),
+    path("tag-create/", CreateTagView.as_view(), name="tags-create"),
+    path("tag-update/<int:pk>/", UpdateTagView.as_view(), name="tags-update"),
+    path("tag-delete/<int:pk>/", DeleteTagView.as_view(), name="tags-delete"),
+
 
 ]
 
