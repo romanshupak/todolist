dbc90a9564ebd35368611ffa6c2781cccee94ea3
Roman Shupak <romanshupak808@gmail.com>
2024-11-19 15:56:04 +0200

    Format with black.

--- tasks/urls.py
+++ tasks/urls.py
@@ -15,12 +15,24 @@ urlpatterns = [
     path("", index, name="index"),
     path("tags/", TagsListView.as_view(), name="tags-list"),
     path("tasks-create/", CreateTaskView.as_view(), name="tasks-create"),
-    path("tasks-update/<int:pk>/", UpdateTaskView.as_view(), name="tasks-update"),
-    path("tasks-delete/<int:pk>/", DeleteTaskView.as_view(), name="tasks-delete"),
+    path(
+        "tasks-update/<int:pk>/",
+        UpdateTaskView.as_view(),
+        name="tasks-update"
+    ),
+    path(
+        "tasks-delete/<int:pk>/",
+        DeleteTaskView.as_view(),
+        name="tasks-delete"
+    ),
     path("tag-create/", CreateTagView.as_view(), name="tags-create"),
     path("tag-update/<int:pk>/", UpdateTagView.as_view(), name="tags-update"),
     path("tag-delete/<int:pk>/", DeleteTagView.as_view(), name="tags-delete"),
-    path("toggle-status/<int:pk>/", ToggleTaskStatusView.as_view(), name="toggle-status"),
+    path(
+        "toggle-status/<int:pk>/",
+        ToggleTaskStatusView.as_view(),
+        name="toggle-status"
+    ),
 
 
 ]
